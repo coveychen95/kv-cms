844a484ed6799171bd44e691a1716e92902cddb2
coveychen95 <coveychen95@outlook.com>
2023-02-10 00:03:50 +0800

    feat:

---
+++ web/src/views/weather/component/BaseModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div v-show="modalValue" class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"></div>
+    </Transition>
+  </Teleport>
+</template>
+
+<script lang="ts" setup>
+  defineEmits(['close-modal'])
+  defineProps({
+    modalValue: {
+      type: Boolean,
+      default: false
+    }
+  })
+</script>
--- web/src/views/weather/component/SiteNavigation.vue
+++ web/src/views/weather/component/SiteNavigation.vue
@@ -2,7 +2,7 @@
   <header class="sticky top-0 shadow-lg bg-weather-primary">
     <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
       <RouterLink :to="{ name: 'home' }" class="flex items-center gap-3">
-        <i class="text-2xl fa-solid fa-sun"></i>
+        <i class="text-2xl fa-solid fa-sun" @click="toggleModal"></i>
         <p class="text-2xl">The Local Weather</p>
       </RouterLink>
       <div class="flex justify-end flex-1 gap-3">
@@ -11,8 +11,19 @@
       </div>
     </nav>
   </header>
+
+  <BaseModal :modalValue="isModal" @closeModal="toggleModal">
+    <div ></div>
+  </BaseModal>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import { ref } from 'vue' 
 import { RouterLink } from 'vue-router'
+import BaseModal from './BaseModal.vue'
+
+const isModal = ref(false)
+const toggleModal = () => {
+  isModal.value = !isModal.value
+}
 </script>
