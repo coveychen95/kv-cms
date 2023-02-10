7e51f0baa90bb5a50d4e786a52932ded94bf410c
gang.chen1 <gang.chen1@yintech.cn>
2023-02-10 15:48:59 +0800

    feat: modal

--- web/src/views/weather/component/BaseModal.vue
+++ web/src/views/weather/component/BaseModal.vue
@@ -1,7 +1,23 @@
 <template>
   <Teleport to="body">
+    
     <Transition name="modal-outer">
-      <div v-show="modalValue" class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"></div>
+      <div
+        v-show="modalValue"
+        class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"
+      >
+        <Transition name="modal-inner">
+          <div v-if="modalValue" class="self-start max-w-screen-md p-4 mt-32 text-black bg-white ">
+            <slot></slot>
+            <button
+              @click="$emit('close-modal')"
+              class="px-6 py-2 mt-8 text-white bg-weather-primary"
+            >Close</button>
+          </div>
+        </Transition>
+        
+      </div>
+      
     </Transition>
   </Teleport>
 </template>
--- web/src/views/weather/component/SiteNavigation.vue
+++ web/src/views/weather/component/SiteNavigation.vue
@@ -2,18 +2,28 @@
   <header class="sticky top-0 shadow-lg bg-weather-primary">
     <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
       <RouterLink :to="{ name: 'home' }" class="flex items-center gap-3">
-        <i class="text-2xl fa-solid fa-sun" @click="toggleModal"></i>
+        <i class="text-2xl fa-solid fa-sun" ></i>
         <p class="text-2xl">The Local Weather</p>
       </RouterLink>
       <div class="flex justify-end flex-1 gap-3">
-        <i class="text-xl duration-150 cursor-pointer fa-solid fa-circle-info hover:text-weather-secondary"></i>
+        <i @click="toggleModal" class="text-xl duration-150 cursor-pointer fa-solid fa-circle-info hover:text-weather-secondary"></i>
         <i class="text-xl duration-150 cursor-pointer fa-solid fa-plus hover:text-weather-secondary"></i>
       </div>
     </nav>
   </header>
 
   <BaseModal :modalValue="isModal" @closeModal="toggleModal">
-    <div ></div>
+    <div>
+      <h1 class="mb-1 text-2xl">About:</h1>
+      <p class="mb-4">The Local Weather allows you to track the current and future weather or cities of your choosing.</p>
+      <h2 class="text-2xl">How it works:</h2>
+      <ol  class="mb-4 list-decimal list-inside">
+        <li>Search for your city by entering the name into the search bar.</li>
+        <li>Select a city by clicking the "+" icon in the top right. This will save the city to view at o later time on the home page.</li>
+      </ol>
+      <h2 class="text-2xl">Removing a city:</h2>
+      <p>If you no longer wish to track a city, simply select the city within the home page. At the bottom of the page, there will be an option to delete the city.</p>
+    </div>
   </BaseModal>
 </template>
 
@@ -27,3 +37,31 @@ const toggleModal = () => {
   isModal.value = !isModal.value
 }
 </script>
+
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3 cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+
+.modal-inner-enter-active {
+  transition: all 0.3 cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
