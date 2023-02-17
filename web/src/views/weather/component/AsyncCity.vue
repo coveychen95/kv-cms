ecbd81d94ab76d4828b00ff6e7c788e022f1243d
coveychen95 <coveychen95@outlook.com>
2023-02-18 01:04:18 +0800

    feat: lesson 10

--- web/src/views/weather/component/AsyncCity.vue
+++ web/src/views/weather/component/AsyncCity.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import axios from 'axios' 
+import axios from 'axios'
 import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
@@ -7,87 +7,99 @@ const router = useRouter()
 
 const getWeatherData = () => {
   // eslint-disable-next-line prettier/prettier
-  return axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
-  ).then((res: any) => {
-    const localOffset = new Date().getTimezoneOffset() * 60000
-    const utc = res.data.current.dt * 1000 + localOffset
-    res.data.currentTime = utc + 1000 * res.data.timezone_offset;
-    res.data.hourly.forEach((hour: any) => {
-      const utc = hour.dt * 1000 + localOffset;
-      hour.currentTime =
-        utc + 1000 * res.data.timezone_offset;
-    });
-    return res.data;
-  }).catch((err: any) => {
-    console.log(err)
-  })
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+    )
+    .then((res: any) => {
+      const localOffset = new Date().getTimezoneOffset() * 60000
+      const utc = res.data.current.dt * 1000 + localOffset
+      res.data.currentTime = utc + 1000 * res.data.timezone_offset
+      res.data.hourly.forEach((hour: any) => {
+        const utc = hour.dt * 1000 + localOffset
+        hour.currentTime = utc + 1000 * res.data.timezone_offset
+      })
+      return res.data
+    })
+    .catch((err: any) => {
+      console.log(err)
+    })
 }
 
 const weatherData = await getWeatherData()
-console.log('weatherData: ', weatherData)
 const STORAGE_KEY_SAVED_CITIES = 'saved-cities'
 const removeCity = () => {
-  const cities = JSON.parse(localStorage.getItem(STORAGE_KEY_SAVED_CITIES));
-  const updatedCities = cities.filter(
-    (city) => city.id !== route.query.id
-  );
-  localStorage.setItem(
-    STORAGE_KEY_SAVED_CITIES,
-    JSON.stringify(updatedCities)
-  );
+  const cities = JSON.parse(localStorage.getItem(STORAGE_KEY_SAVED_CITIES))
+  const updatedCities = cities.filter((city) => city.id !== route.query.id)
+  localStorage.setItem(STORAGE_KEY_SAVED_CITIES, JSON.stringify(updatedCities))
   router.push({
-    name: "weather",
-  });
+    name: 'weather',
+  })
 }
 </script>
 
 <template>
-  <div class="flex flex-col items-center flex-1 ">
-    <div v-if="$route.query.preview" class="w-full p-4 text-center text-white bg-weather-secondary">
-      <p>You are currently previewing this city, click the "+" to start tracking this city.</p>
+  <div class="flex flex-col items-center flex-1">
+    <div
+      v-if="$route.query.preview"
+      class="w-full p-4 text-center text-white bg-weather-secondary"
+    >
+      <p>
+        You are currently previewing this city, click the "+" to start tracking
+        this city.
+      </p>
     </div>
     <div class="flex flex-col items-center py-12 text-white">
       <h1 class="mb-2 text-4xl">{{ $route.params.city.split('-')[0] }}</h1>
       <p class="mb-12 text-sm">
-        {{ 
+        {{
           new Date(weatherData.currentTime).toLocaleString('en-us', {
             weekday: 'short',
             day: '2-digit',
-            month: 'long'
+            month: 'long',
           })
         }}
-        {{ 
-          new Date(weatherData.currentTime).toLocaleTimeString("en-us",{
-            timeStyle: "short",
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString('en-us', {
+            timeStyle: 'short',
           })
         }}
       </p>
-      <p class="mb-8 text-8xl">&nbsp;{{ Math.round(weatherData.current.temp) }}&deg;</p>
+      <p class="mb-8 text-8xl">
+        &nbsp;{{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
       <p>Feels like {{ Math.round(weatherData.current.feels_like) }}&deg;</p>
       <p class="capitalize">{{ weatherData.current.weather[0].description }}</p>
-      <img :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@4x.png`" class="w-[150px] h-auto" alt="">
+      <img
+        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@4x.png`"
+        class="w-[150px] h-auto"
+        alt=""
+      />
     </div>
-    <hr class="w-full border border-white border-opacity-10">
+    <hr class="w-full border border-white border-opacity-10" />
     <!-- Hourly Weather -->
     <div class="w-full max-w-screen-md py-12">
       <div class="mx-8 text-white">
         <h2 class="mb-4">Hourly Weather</h2>
         <div class="flex gap-10 overflow-x-scroll">
-          <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col items-center gap-4">
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="flex flex-col items-center gap-4"
+          >
             <p class="whitespace-nowrap text-md">
-              {{ 
-                new Date(
-                  hourData.currentTime
-                ).toLocaleTimeString("en-us", {
-                  hour: "numeric",
-                }) 
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString('en-us', {
+                  hour: 'numeric',
+                })
               }}
             </p>
-            <img class="w-auto h-[50px] object-cover" :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`" alt="">
-            <p class="text-xl">
-              {{ Math.round(hourData.temp) }}&deg;
-            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt=""
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
           </div>
         </div>
       </div>
@@ -96,22 +108,23 @@ const removeCity = () => {
     <div class="w-full max-w-screen-md py-12">
       <div class="mx-8 text-white">
         <h2 class="mb-4">7 Day Forecast</h2>
-        <div 
-          v-for="day in weatherData.daily" 
-          :key="day.dt" 
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
           class="flex items-center"
         >
           <p class="flex-1">
-            {{ 
-              new Date(day.dt * 1000).toLocaleDateString(
-                "en-us",
-                {
-                  weekday: "long",
-                }
-              )
+            {{
+              new Date(day.dt * 1000).toLocaleDateString('en-us', {
+                weekday: 'long',
+              })
             }}
           </p>
-          <img class="w-[50px] h-[50px] object-cover" :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="">
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
           <div class="flex justify-end flex-1 gap-2">
             <p>H: {{ Math.round(day.temp.max) }}</p>
             <p>L: {{ Math.round(day.temp.min) }}</p>
--- web/src/views/weather/component/SiteNavigation.vue
+++ web/src/views/weather/component/SiteNavigation.vue
@@ -1,13 +1,22 @@
 <template>
   <header class="sticky top-0 shadow-lg bg-weather-primary">
-    <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
+    <nav
+      class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row"
+    >
       <RouterLink :to="{ name: 'home' }" class="flex items-center gap-3">
-        <i class="text-2xl fa-solid fa-sun" ></i>
+        <i class="text-2xl fa-solid fa-sun"></i>
         <p class="text-2xl">The Local Weather</p>
       </RouterLink>
       <div class="flex justify-end flex-1 gap-3">
-        <i @click="toggleModal" class="text-xl duration-150 cursor-pointer fa-solid fa-circle-info hover:text-weather-secondary"></i>
-        <i class="text-xl duration-150 cursor-pointer fa-solid fa-plus hover:text-weather-secondary"></i>
+        <i
+          @click="toggleModal"
+          class="text-xl duration-150 cursor-pointer fa-solid fa-circle-info hover:text-weather-secondary"
+        ></i>
+        <i
+          @click="addCity"
+          v-if="route.query"
+          class="text-xl duration-150 cursor-pointer fa-solid fa-plus hover:text-weather-secondary"
+        ></i>
       </div>
     </nav>
   </header>
@@ -15,22 +24,68 @@
   <BaseModal :modalValue="isModal" @closeModal="toggleModal">
     <div>
       <h1 class="mb-1 text-2xl">About:</h1>
-      <p class="mb-4">The Local Weather allows you to track the current and future weather or cities of your choosing.</p>
+      <p class="mb-4">
+        The Local Weather allows you to track the current and future weather or
+        cities of your choosing.
+      </p>
       <h2 class="text-2xl">How it works:</h2>
-      <ol  class="mb-4 list-decimal list-inside">
+      <ol class="mb-4 list-decimal list-inside">
         <li>Search for your city by entering the name into the search bar.</li>
-        <li>Select a city by clicking the "+" icon in the top right. This will save the city to view at o later time on the home page.</li>
+        <li>
+          Select a city by clicking the "+" icon in the top right. This will
+          save the city to view at o later time on the home page.
+        </li>
       </ol>
       <h2 class="text-2xl">Removing a city:</h2>
-      <p>If you no longer wish to track a city, simply select the city within the home page. At the bottom of the page, there will be an option to delete the city.</p>
+      <p>
+        If you no longer wish to track a city, simply select the city within the
+        home page. At the bottom of the page, there will be an option to delete
+        the city.
+      </p>
     </div>
   </BaseModal>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue' 
-import { RouterLink } from 'vue-router'
+import { reactive, ref } from 'vue'
+import { RouterLink, useRoute, useRouter } from 'vue-router'
+import { uid } from 'uid'
 import BaseModal from './BaseModal.vue'
+const STORAGE_KEY_SAVED_CITIES: string = 'saved-cities'
+
+const route = useRoute()
+const router = useRouter()
+
+const savedCities = ref<Array<any>>([])
+
+const addCity = () => {
+  const localCities = localStorage.getItem(STORAGE_KEY_SAVED_CITIES)
+  if (localCities) {
+    savedCities.value = JSON.parse(localCities)
+  }
+  const [city, state] = (route.params.city as string).split('-')
+  const locationObj = {
+    id: uid(),
+    state,
+    city,
+    coords: {
+      lat: route.query.lat,
+      lng: route.query.lng,
+    },
+  }
+
+  savedCities.value.push(locationObj)
+
+  localStorage.setItem(
+    STORAGE_KEY_SAVED_CITIES,
+    JSON.stringify(savedCities.value)
+  )
+
+  let query = { ...route.query }
+  delete query.preview
+  query.id = locationObj.id
+  router.replace({ query })
+}
 
 const isModal = ref(false)
 const toggleModal = () => {
@@ -48,7 +103,6 @@ const toggleModal = () => {
 .modal-outer-leave-to {
   opacity: 0;
 }
-
 
 .modal-inner-enter-active {
   transition: all 0.3 cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
